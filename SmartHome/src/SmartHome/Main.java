992f421bd355783efbc9fed92dc22be392199a2a
Lucas de Souza Tito <ltito@appi.corp.local>
2017-11-19 23:31:09 -0200

    minha parte

---
+++ SmartHome/src/SmartHome/Main.java
@@ -0,0 +1,10 @@
+package SmartHome;
+
+public class Main {
+
+	public static void main(String[] args) {
+Historic historic = new Historic();
+
+	}
+
+}
---
+++ SmartHome/src/SmartHome/SmartHome.java
@@ -0,0 +1,26 @@
+package SmartHome;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class SmartHome {
+
+	/*public static Connection connection = null;
+private final static String pathdb = "jdbc:mysql://localhost/smarthome";
+
+public static void main(String[] args) throws SQLException {
+Historic historic = new Historic();
+System.out.println("qual é o usuário que você quer ver o histórico?");
+Scanner scan = new Scanner(System.in);
+historic.printHistoric(scan.nextInt());
+	try {
+			connection = DriverManager.getConnection(pathdb);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+	*/}
+
+}
